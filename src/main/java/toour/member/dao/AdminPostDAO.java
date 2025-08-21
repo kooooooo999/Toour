13679f13e872f253517550dc42e798772bea25ca
cornsoupp <qwpo2848@gmail.com>
2025-08-21 17:49:59 +0900

    보은이 수정함

--- src/main/java/toour/member/dao/AdminPostDAO.java
+++ src/main/java/toour/member/dao/AdminPostDAO.java
@@ -247,6 +247,32 @@ public class AdminPostDAO {
         }
         return filecnt;
     }
+
+    //전체가져오기는 그냥 가져오면 될거같은데?
+    public static PostVO[] getCommentReport(String target_type,String begin, String end){
+        //rnum, report_idx, reporter_idx, reported_idx, post_idx, comment_idx, report_content, report_created_at, target_type,
+        // report_status, comment_content, post_title, reporter_nickname, reported_nickname
+
+        PostVO[] ar = null;
+        Map<String,String> map = new HashMap<>();
+        if(target_type == "COMMENT")
+            map.put("target_type",target_type);
+        map.put("begin",begin);
+        map.put("end",end);
+
+
+        SqlSession ss = FactoryService.getFactory().openSession();
+
+        List<PostVO> list = ss.selectList("adminpost.selectReportComment",map);
+        if(list.size()>0&& !list.isEmpty()){
+            ar = new PostVO[list.size()];
+            list.toArray(ar);
+        }
+        ss.close();
+        return ar;
+    }
+
+
 }
 
 //    // 조회수 증가
--- src/main/webapp/common/adminSidebar.jsp
+++ src/main/webapp/common/adminSidebar.jsp
@@ -26,7 +26,6 @@
   <a href="Controller">🛖 메인 홈</a>
   <a href="AdminController?type=adminnotice">📢 공지사항 관리</a>
   <a href="AdminController?type=adminpost">📝 게시물 관리</a>
-  <a href="AdminController?type=admincomment">💬 댓글 관리</a>
   <a href="AdminController?type=adminmemlist">👥 회원정보 관리</a>
   <a href="AdminController?type=adminInquiry&pageType=inquiry">📬문의사항 관리</a>
   <a href="AdminController?type=adminReport">🚨 신고사항 관리</a>
